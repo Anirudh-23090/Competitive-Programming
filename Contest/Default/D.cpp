49c78ce5e20f104ebb946b5dd9d5f9f0558ef1c5
Anirudh-23090 <anirudh23090@iiitd.ac.in>
2024-11-12 01:46:58 +0530

    practice and contest folder added

---
+++ Contest/Default/D.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+void solve() {
+    
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+    
+    return 0;
+}
